1b4933d51730afb953aa5f1a06ce4602bf61ba02
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-10-26 09:33:42 +0530

    init

---
+++ server/customer management/src/test/java/com/supermarket/customer/customer_management/CustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket.customer.customer_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
