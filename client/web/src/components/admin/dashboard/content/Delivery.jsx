99ae9343e0d228a23c56aa40682788099a478224
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-11-06 13:43:51 +0530

    delivery

--- client/web/src/components/admin/dashboard/content/Delivery.jsx
+++ client/web/src/components/admin/dashboard/content/Delivery.jsx
@@ -12,6 +12,7 @@ const Delivery = () => {
       <div className="delivery-list">
         {deliveryPersons.map((item,index)=>(
           <div className="delivery-item" key={index}>
+          <div className="personal">
           <div className="avatar">
             <img src={Avatar}/>
           </div>
@@ -20,6 +21,16 @@ const Delivery = () => {
             <div className="email">johndoe@gmail.com</div>
             <div className="contact">071 123 4562</div>
           </div>
+          </div>
+
+          <div className="status">
+            Status: <span>{item.status}</span>
+          </div>
+
+          <div className="options">
+              <div className="btn option"><span>View on Map</span> </div>
+              {/* <div className="btn option" ><span>View</span> </div> */}
+              </div>
         </div>
         ))}
       </div>
