e59ff9970dc873fcdc102e77bb6c79988f72299d
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-11-06 22:24:41 +0530

    baseUrl

---
+++ client/web/src/Apis/routeCollection.js
@@ -0,0 +1,13 @@
+const route_collection = {
+    customerIncome:'/getCustomerIncome',
+    developerRevenue:'/getDeveloperRevenue',
+    developerWithdrawal:'/getDeveloperWithdrawal',
+    sendNotification:'/pushNotification',
+    getNotification: '/getNotification',
+    submitComplain:'/submitComplain',
+    getComplains:'/getComplains',
+    reviewComplain:'/markReviewComplain',
+    getTopRankedGames:'getRankedGames'
+}
+
+export default route_collection 
