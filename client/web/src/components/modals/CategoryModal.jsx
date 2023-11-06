b86c73a3bed143820f04c8678e063e8ac57c7846
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-11-06 22:58:47 +0530

    push#2

--- client/web/src/components/modals/CategoryModal.jsx
+++ client/web/src/components/modals/CategoryModal.jsx
@@ -6,8 +6,23 @@ import ImageUpload from "../shared/ImageUpload";
 const CategoryModal = ({ closeModal, data }) => {
   const [status, setStatus] = useState(-10);
   const [progress, setProgress] = useState(0);
-  const imageRef = useRef(null);
+  const fileInputRef = useRef(null);
+  const [file, setFile] = useState();
 
+
+  const handleFileChange = (event) => {
+    const newFile = event.target.files[0];
+    
+    if (newFile) {
+      // Handle the selected file here
+      setFile(newFile);
+      setProgress(0);
+    }
+  };
+
+  const handleImageSelect = () => {
+    fileInputRef.current.click();
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -26,13 +41,27 @@ const CategoryModal = ({ closeModal, data }) => {
 
       <div className="modal-body">
         <div className="left">
-          {data.image ? (
-            <img src={data.image} />
+         <div className="figure" onClick={handleImageSelect}>
+         {data.image || file ? (
+            <div><img ref={fileInputRef}  src={URL.createObjectURL(file) || data.image} /></div>
           ) : (
-            <div onClick={() => console.log("hehe")}>
+            <div ref={fileInputRef}>
               <ImagePlaceHolder />
+              
+              
             </div>
           )}
+         </div>
+
+<input
+              type="file"
+              ref={fileInputRef}
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={handleFileChange}
+              
+            />
+
          {status>=0 && status <=100 &&  <ImageUpload
             filename={"Test file. jpg"}
             size={"15MB"}
