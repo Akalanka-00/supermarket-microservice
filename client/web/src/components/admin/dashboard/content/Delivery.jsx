bf6879bcb66231fd9326b26c895f8fa5ca22ec21
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-11-06 13:21:11 +0530

    update

--- client/web/src/components/admin/dashboard/content/Delivery.jsx
+++ client/web/src/components/admin/dashboard/content/Delivery.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import Avatar from "../../../../assets/icons/profile.png"
+import deliveryPersons from '../../../../context/DeliveryPersons'
 const Delivery = () => {
   return (
-    <div>
-      Delivery
-    </div>
+    <section className='delivery-container' >
+      <div className="header">
+        <div className="title">
+          Delivers
+        </div>
+      </div>
+      <div className="delivery-list">
+        {deliveryPersons.map((item,index)=>(
+          <div className="delivery-item" key={index}>
+          <div className="avatar">
+            <img src={Avatar}/>
+          </div>
+          <div className="info">
+            <div className="name">John Doe</div>
+            <div className="email">johndoe@gmail.com</div>
+            <div className="contact">071 123 4562</div>
+          </div>
+        </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
