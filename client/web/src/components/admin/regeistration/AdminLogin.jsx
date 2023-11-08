262acbc6b641babf1a60384b3cf0481cb7c084b1
Shakthi Jayasinghe <97536701+sumudushakthi@users.noreply.github.com>
2023-11-08 12:10:04 +0530

    add some form validation to customer registration and login and admin login but API calling parts are commented

--- client/web/src/components/admin/regeistration/AdminLogin.jsx
+++ client/web/src/components/admin/regeistration/AdminLogin.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
 import DeliveryPerson from "../../../assets/images/deliveryPerson1.jpg"
 import LOGO from "../../../assets/icons/logo.png"
 import "../../../styles/customer/login.css"
 import { useNavigate } from 'react-router-dom'
 const AdminLogin = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [emailValid, setEmailValid] = useState(true);
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (event) => {
+      const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i; // A basic regex for email validation
+      //const email = event.target.value;
+  
+      if (emailRegex.test(email)) {
+        setEmailValid(true);
+        setEmail(event.target.value);
+      } else {
+        setEmailValid(false);
+      }
+    };
+
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+
+    // const handleSubmit = async (event) => {
+    //   event.preventDefault();
+  
+    //   try {
+    //     // Send a POST request to your backend with the email and password
+    //     const response = await fetch('/api/admin/login', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({ email, password }),
+    //     });
+  
+    //     if (response.status === 200) {
+    //       // Successfully logged in, you can redirect to the admin dashboard
+    //       navigate("/admin/dashboard");
+    //     } else {
+    //       // Handle login failure, show an error message to the user, e.g., incorrect email or password
+    //     }
+    //   } catch (error) {
+    //     // Handle network or server errors
+    //   }
+    // };
+
   return (
     <div className='customer-login-container'>
       <div className="left">
@@ -22,8 +66,8 @@ const AdminLogin = () => {
       </div>
 
       <form action="" className="login-form" onSubmit={()=>navigate("/admin/dashboard")}>
-        <input type="email" className="field" placeholder='Enter your email'/>
-        <input type="password" className="field" placeholder='Enter your password'/>
+        <input type="email" className="field" placeholder='Enter your email' onChange={handleEmailChange}/>
+        <input type="password" className="field" placeholder='Enter your password'onChange={handlePasswordChange}/>
         <button type="submit" className="btn input-btn" >Login</button>
       </form>
       </div>
--- client/web/src/components/customer/regeistration/CustomerRegister.jsx
+++ client/web/src/components/customer/regeistration/CustomerRegister.jsx
@@ -1,10 +1,90 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import DeliveryPerson from "../../../assets/images/deliveryPerson2.jpg"
 import LOGO from "../../../assets/icons/logo.png"
 import "../../../styles/customer/login.css"
+import "../../../styles/error.css"
 import { useNavigate } from 'react-router-dom'
+
+
+
 const CustomerRegister = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmPassword] = useState('');
+
+    const [emailError, setEmailError] = useState('');
+    const [phoneError, setPhoneError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [passwordMatchError, setPasswordMatchError]=useState('')
+
+
+    const isEmailValid = (email) => {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      return emailRegex.test(email);
+    };
+
+    const isPhoneNumberValid = (phoneNumber) => {
+      const phoneRegex = /^\d{10}$/; // Change the regex to match the desired phone number format
+      return phoneRegex.test(phoneNumber);
+    };
+
+
+    const handleEmailChange = (e) => {
+      const newEmail = e.target.value;
+      setEmail(newEmail);
+      if (!isEmailValid(newEmail)) {
+        setEmailError('Invalid email format');
+      } else {
+        setEmailError('');
+      }
+    };
+
+    const handlePhoneChange = (e) => {
+      const newPhoneNumber = e.target.value.replace(/\D/g, ''); // Remove non-numeric characters
+      setPhoneNumber(newPhoneNumber);
+      if (!isPhoneNumberValid(newPhoneNumber)) {
+        setPhoneError('Invalid phone number format');
+      } else {
+        setPhoneError('');
+      }
+    };
+
+    const handlePasswordChange = (e) => {
+      const newPassword = e.target.value;
+      setPassword(newPassword);
+      if (newPassword.length < 6) {
+        setPasswordError('Password must be at least 6 characters long');
+      } else {
+        setPasswordError('');
+      }
+    };
+
+    const handleConfirmPasswordChange = (e) => {
+      const newConfirmPassword = e.target.value;
+      setConfirmPassword(newConfirmPassword);
+      if (newConfirmPassword !== password) {
+        setPasswordMatchError('Passwords do not match');
+      } else {
+        setPasswordMatchError('');
+      }
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      if (isEmailValid(email) && isPhoneNumberValid(phoneNumber) && password.length >= 6 && password===confirmpassword)  {
+        // Form is valid, you can proceed with registration
+        
+        // For example, navigate to the next page
+        navigate('/customer/login');
+      } else {
+        // Handle validation errors
+        
+      }
+    };
+
   return (
     <div>
        <div className='customer-login-container'>
@@ -22,18 +102,22 @@ const CustomerRegister = () => {
         <div><span>S</span>elect</div> <div><span>B</span>uy</div> <div><span>D</span>eliver</div>
       </div>
 
-      <form action="post" className="login-form" onSubmit={()=>{
-        navigate("/customer/otp")
-      }}>
+      <form action="post" className="login-form" onSubmit={handleSubmit}>
         <div className="input-names">
-        <input type="text" className="field" placeholder='First Name'/>
-        <input type="text" className="field" placeholder='Last Name'/>
+        <input type="text"  className="field" required placeholder='First Name'/>
+        <input type="text" className="field" required placeholder='Last Name'/>
         </div>
-        <input type="email" className="field" placeholder='Enter your email'/>
-        <input type="tel" className="field" placeholder='Contact number'/>
+        <input type="email" className="field" required placeholder='Enter your email'onChange={handleEmailChange}/>
+        <input type="tel" className="field" required placeholder='Contact number'onChange={handlePhoneChange}/>
 
-        <input type="password" className="field" placeholder='Enter your password'/>
-        <input type="password" className="field" placeholder='Re-enter your password'/>
+        <input type="password" className="field" required placeholder='Enter your password' onChange={handlePasswordChange}/>
+        <input type="password" className="field" required placeholder='Re-enter your password' onChange={handleConfirmPasswordChange}/>
+
+        {emailError && <p className="error-message">{emailError}</p>}
+        {phoneError && <p className="error-message">{phoneError}</p>}
+        {passwordError && <p className='error-message'>{passwordError}</p>}
+        {passwordMatchError && <p className='error-message'>{passwordMatchError}</p>}
+
         <button type="submit" className="btn input-btn" >Register</button>
         <div className="login-options">Already have an FLashMart account? <span onClick={()=>navigate("/customer/login",{state:{email:"testmail@gmail.com"}})}>Login</span></div>
       </form>
